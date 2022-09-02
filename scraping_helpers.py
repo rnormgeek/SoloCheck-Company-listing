3d8ee478901dcbaaaa2f81419802d542c9372dbf
Pierre A <rnormgeek@gmail.com>
2022-09-02 12:00:31 +0100

    changed functions to make it easier to add new elements to scrap for

--- scraping_helpers.py
+++ scraping_helpers.py
@@ -29,7 +29,6 @@ def get_tag_from_url_and_id(session, url, id):
     soup = BeautifulSoup(html_content, 'html.parser')
     # Get the text from element id
     results = soup.find(id=id)
-
     return results
 
 # Get company listings complete links
@@ -68,7 +67,6 @@ def get_company_urls(session, url):
     complete_links = [f'{root_url}{link}' for link in results]
 
     return complete_links
-    
 
 class Company:
     def __init__(self, url):
@@ -82,18 +80,36 @@ class Company:
         address_elements = unformated_address.split(",")
         formated_address = " ".join(address_elements)
         return formated_address
-
-    def _get_company_report(self):
+    
+    def _get_element_from_soup(self, soup, id):
         """
-        Get the company report
+        Get the element from the soup
+        """
+        element = soup.find(id=id)
+        logging.debug(f'{id} found')
+        return element
+    
+    def _get_soup(self):
+        response = requests.get(self.url)
+        html_content = response.content
+        # Parse the html content
+        soup = BeautifulSoup(html_content, 'html.parser')
+        logging.debug(f'Parsing complete.')
+        return soup
+
+    def _get_company_summary(self):
+        """
+        Get the company summary
         """
         # Get the html from the url
         response = requests.get(self.url)
         html_content = response.content
         # Parse the html content
         soup = BeautifulSoup(html_content, 'html.parser')
+        logging.debug(f'Parsing complete.')
         # Get the content from the tag
-        tag = soup.find('div', id='report-1')
+        tag = soup.find('div', id='report-6')
+        logging.debug(f'report-6 found')
         return tag
 
     def _get_vitals_from_report(self):
@@ -101,7 +117,9 @@ class Company:
         Using the company report, extract all the vital info of the company
         """
         vitals = {}
-        for ul in self._get_company_report().find_all('ul'):
+        soup = self._get_soup()
+        company_report = self._get_element_from_soup(soup, 'report-1')
+        for ul in company_report.find_all('ul'):
             for li in ul.find_all('li'):
                 for span in li.find_all('span'):
                     if span.has_attr('class'):
@@ -113,6 +131,7 @@ class Company:
                             vitals[key] = value
                         except UnboundLocalError:
                             pass
+        logging.debug(f'Company vitals successfully extracted')
         return vitals
 
     def set_company_attributes(self):
@@ -133,11 +152,20 @@ class Company:
         for v in ['name', 'age', 'company_number', 'size', 'current_status', 'activity', 'trading_name', 'address']:
             self.show(v)
 
+    def write_to_csv(self, filename):
+        df = pd.DataFrame([self.__dict__])
+        df.to_csv(filename, mode='a', header=False, index=False)
+
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG, 
+        format='%(asctime)s - %(levelname)s - %(message)s', 
+        filename='scraping_helpers.log',
+        filemode='a')
+
     session = setup_session(proxies)
-    # print(get_company_listings_complete_links(session))
-    # print(get_company_urls(session, "https://www.solocheck.ie/IrishCompanyInfo?i=00"))
     sample_company_url = 'https://www.solocheck.ie/Irish-Company/N-Oconnor-Construction-Limited-635031'
  
     test_company = Company(url=sample_company_url)
     test_company.show_vitals()
+    print(test_company.__dict__)
